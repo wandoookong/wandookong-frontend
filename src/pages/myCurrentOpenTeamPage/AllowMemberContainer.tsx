08d6cfba6c31b85b64c424e2820a71ac6d37ebac
Geunsu Ryu <linusdamyo@gmail.com>
2022-09-06 20:52:46 +0900

    현재 오픈된 완두콩 작업중..

---
+++ src/pages/myCurrentOpenTeamPage/AllowMemberContainer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { AllowMemberReturnType } from "../../api/types/teamMemberType";
+import AllowMember from "./components/AllowMember";
+
+type AllowMemberContainerContextType = {
+  allowMember: AllowMemberReturnType;
+};
+
+const initialState: AllowMemberContainerContextType = {
+  allowMember: {} as AllowMemberReturnType,
+};
+
+export const AllowMemberContainerContext = React.createContext(initialState);
+
+export default function AllowMemberContainer(allowMember: AllowMemberReturnType) {
+  return (
+    <AllowMemberContainerContext.Provider value={{ allowMember }}>
+      <AllowMember />
+    </AllowMemberContainerContext.Provider>
+  );
+}
--- src/pages/myCurrentOpenTeamPage/MyCurrentOpenTeamPage.tsx
+++ src/pages/myCurrentOpenTeamPage/MyCurrentOpenTeamPage.tsx
@@ -1,39 +1,88 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../components/layout/layout";
-import UserApi from "../../api/userApi";
-import { UserMeReturnType } from "../../api/types/userType";
-import ApplyMemberList from "./components/ApplyMemberList";
-import AllowMemberList from "./components/AllowMemberList";
+import { CurrentOpenTeamReturnType } from "../../api/types/teamType";
+import MyTeamApi from "../../api/myTeamApi";
+import TeamMemberApi from "../../api/teamMemberApi";
+import { AllowMemberListReturnType, ApplyMemberListReturnType } from "../../api/types/teamMemberType";
+import ApplyMemberContainer from "./ApplyMemberContainer";
+import AllowMemberContainer from "./AllowMemberContainer";
 
-type MyCurrentOpenTeamPageContextType = {
-  isLoading: boolean;
-  meInfo: UserMeReturnType;
-};
-
-const initialState: MyCurrentOpenTeamPageContextType = {
-  isLoading: false,
-  meInfo: {} as UserMeReturnType,
-};
-
-export const MyCurrentOpenTeamPageContext = React.createContext(initialState);
+export type PageModeType = "apply" | "allow";
 
 export default function MyCurrentOpenTeamPage() {
   const [isLoading, setIsLoading] = useState(true);
-  const [meInfo, setMeInfo] = useState({} as UserMeReturnType);
+  const [pageMode, setPageMode] = useState<PageModeType>("apply");
+  const [currentOpenTeam, setCurrentOpenTeam] = useState({} as CurrentOpenTeamReturnType);
+  const [applyMemberList, setApplyMemberList] = useState({} as ApplyMemberListReturnType);
+  const [allowMemberList, setAllowMemberList] = useState({} as AllowMemberListReturnType);
 
   useEffect(() => {
-    UserApi.getUserMe().then((res) => {
-      setMeInfo(res);
-      setIsLoading(false);
-    });
+    Promise.all([MyTeamApi.getCurrentOpenTeam(), TeamMemberApi.getApplyMemberList()])
+      .then(([currentOpenTeamRes, applyMemberListRes]) => {
+        setCurrentOpenTeam(currentOpenTeamRes);
+        setApplyMemberList(applyMemberListRes);
+      })
+      .finally(() => setIsLoading(false));
   }, []);
+
+  const onClickTab = (e, pageMode: PageModeType) => {
+    e.preventDefault();
+
+    setPageMode(pageMode);
+
+    if (pageMode === "apply") {
+      getApplyList();
+    } else {
+      getAllowList();
+    }
+  };
+
+  const getApplyList = () => {
+    setIsLoading(true);
+    TeamMemberApi.getApplyMemberList()
+      .then((res) => {
+        setApplyMemberList(res);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const getAllowList = () => {
+    setIsLoading(true);
+    TeamMemberApi.getAllowMemberList()
+      .then((res) => {
+        setAllowMemberList(res);
+      })
+      .finally(() => setIsLoading(false));
+  };
 
   return (
     <Layout>
-      <MyCurrentOpenTeamPageContext.Provider value={{ isLoading, meInfo }}>
-        <ApplyMemberList />
-        <AllowMemberList />
-      </MyCurrentOpenTeamPageContext.Provider>
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          <div>
+            <div>{currentOpenTeam.teamCategory === "portfolio" ? "포트폴리오" : "사이드 프로젝트"}</div>
+            <div>{currentOpenTeam.closeDueYmd}</div>
+            <div>{currentOpenTeam.title}</div>
+          </div>
+          <div>
+            <button onClick={(e) => onClickTab(e, "apply")}>신청자</button>
+            {pageMode === "apply" && <div>----</div>}
+            <button onClick={(e) => onClickTab(e, "allow")}>참여자</button>
+            {pageMode === "allow" && <div>----</div>}
+          </div>
+          <>
+            {pageMode === "apply" ? (
+              applyMemberList.list.map((applyMember) => <ApplyMemberContainer {...applyMember} />)
+            ) : pageMode === "allow" ? (
+              allowMemberList.list.map((allowMember) => <AllowMemberContainer {...allowMember} />)
+            ) : (
+              <div>404</div>
+            )}
+          </>
+        </>
+      )}
     </Layout>
   );
 }
