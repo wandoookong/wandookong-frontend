72d1b41c7d8e03b69483349355687f49922bb762
ireneworks <hello@ireneworks.com>
2023-01-10 16:47:10 +0900

    updated QA issues

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,27 @@ import MyJoinedTeams from "./pages/myPages/myJoinedTeams/MyJoinedTeams";
 import MyTeamHistoryPage from "./pages/myPages/myTeamHistoryPage/MyTeamHistoryPage";
 import ConditionTerms from "./pages/terms/conditionTerms";
 import PrivacyTerms from "./pages/terms/privacyTerms";
+import RequireAuth from "./pages/configuration/requireAuth";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<RequireAuth />}>
+          <Route path="team/:teamId/apply" element={<ApplyTeam />} />
+          <Route path="request" element={<TeamRequestForm />} />
+          <Route path="myAccount" element={<MyPage />} />
+          <Route path="my-profile" element={<MyProfileEdit />} />
+          <Route path="myCreatedTeam" element={<MyCurrentCreatedTeam />} />
+          <Route path="my-team-history" element={<MyTeamHistoryPage />} />
+          <Route path="my-team-party" element={<MyJoinedTeams />} />
+        </Route>
         <Route path="/privacy" element={<PrivacyTerms />} />
         <Route path="/terms" element={<ConditionTerms />} />
-        <Route path="/team/:teamId/apply" element={<ApplyTeam />} />
         <Route path="/team/:teamId" element={<TeamDetail />} />
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
-        <Route path="/request" element={<TeamRequestForm />} />
-        <Route path="/myAccount" element={<MyPage />} />
-        <Route path="/my-profile" element={<MyProfileEdit />} />
-        <Route path="/myCreatedTeam" element={<MyCurrentCreatedTeam />} />
-        <Route path="/my-team-history" element={<MyTeamHistoryPage />} />
-        <Route path="/my-team-party" element={<MyJoinedTeams />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
