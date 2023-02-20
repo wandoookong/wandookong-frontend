42429e44f4975b1af273194bb7f37932cc893e28
ireneworks <hello@ireneworks.com>
2023-02-20 20:58:03 +0900

    refactor QA

--- src/api/config/interceptors/request/accessTokenAuthenticationInterceptor.ts
+++ src/api/config/interceptors/request/accessTokenAuthenticationInterceptor.ts
@@ -1,19 +1,23 @@
 import { AxiosRequestConfig } from "axios";
 import { ACCESS_TOKEN_NAME } from "../../config";
 
-const requiredAccessTokenUrlWhiteList = ["/my-teams/current-open", "/users/me", "/pre-create"];
+const requiredAccessTokenUrlWhiteList = [
+  "/my-teams/current-open",
+  "/users/me",
+  "/pre-create",
+  "/team-members/allow",
+  "/team-members/apply",
+];
 
 export const accessTokenAuthenticationInterceptor = {
   onFulfilled: (config: AxiosRequestConfig) => {
-    const isRequiredAccessTokenUrl = requiredAccessTokenUrlWhiteList.some((whiteListUrl) => {
-      return config.url?.includes(whiteListUrl);
-    });
-    if (isRequiredAccessTokenUrl) {
-      const accessTokenFromStorage = localStorage.getItem(ACCESS_TOKEN_NAME);
-      if (accessTokenFromStorage) {
-        config.headers!.Authorization = `Bearer ${accessTokenFromStorage}`;
-        return config;
-      }
+    // const isRequiredAccessTokenUrl = requiredAccessTokenUrlWhiteList.some((whiteListUrl) => {
+    //   return config.url?.includes(whiteListUrl);
+    // });
+    const accessTokenFromStorage = localStorage.getItem(ACCESS_TOKEN_NAME);
+    if (accessTokenFromStorage) {
+      config.headers = { Authorization: `Bearer ${accessTokenFromStorage}` };
+    } else {
       return (document.location.href = "/login");
     }
     return config;
