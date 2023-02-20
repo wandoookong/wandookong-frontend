70c1241b640869b3f41a51239861aa89cac4674c
ireneworks <hello@ireneworks.com>
2023-02-20 21:10:48 +0900

    refactor QA

--- src/api/config/interceptors/request/accessTokenAuthenticationInterceptor.ts
+++ src/api/config/interceptors/request/accessTokenAuthenticationInterceptor.ts
@@ -1,19 +1,8 @@
 import { AxiosRequestConfig } from "axios";
 import { ACCESS_TOKEN_NAME } from "../../config";
 
-const requiredAccessTokenUrlWhiteList = [
-  "/my-teams/current-open",
-  "/users/me",
-  "/pre-create",
-  "/team-members/allow",
-  "/team-members/apply",
-];
-
 export const accessTokenAuthenticationInterceptor = {
   onFulfilled: (config: AxiosRequestConfig) => {
-    // const isRequiredAccessTokenUrl = requiredAccessTokenUrlWhiteList.some((whiteListUrl) => {
-    //   return config.url?.includes(whiteListUrl);
-    // });
     const accessTokenFromStorage = localStorage.getItem(ACCESS_TOKEN_NAME);
     if (accessTokenFromStorage) {
       config.headers!.Authorization = `Bearer ${accessTokenFromStorage}`;
